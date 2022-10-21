e3e588c4d97802c700944ebed24ebd6f7998a684
Guillermo Cava <gcavanunez@gmail.com>
2022-10-20 19:04:02 -0500

    big bang

---
+++ src/routes/other.tsx
@@ -0,0 +1,27 @@
+import { createSignal, Show } from 'solid-js';
+import { Title } from 'solid-start';
+import Counter from '~/components/Counter';
+
+export default function Home() {
+  const [visible, setVisible] = createSignal(true);
+
+  return (
+    <main>
+      <Title>Other World</Title>
+      <Show when={visible()}>
+        <div>
+          <Title>Title 2</Title>
+        </div>
+      </Show>
+      <h1>Hello world!</h1>
+      <Counter />
+      <p>
+        Visit{' '}
+        <a href="https://start.solidjs.com" target="_blank">
+          start.solidjs.com
+        </a>{' '}
+        to learn how to build SolidStart apps.
+      </p>
+    </main>
+  );
+}
